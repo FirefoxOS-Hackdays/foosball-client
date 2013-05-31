2c634e1ee37b7e85504da949927402755914c05e
Rafael de Oleza <rafeca@gmail.com>
2013-06-01 00:39:53 +0100

    Add shake logic

--- desktop/js/ws.js
+++ desktop/js/ws.js
@@ -1,13 +1,14 @@
 
 var WsConnector = new function () {
 
-  var socket = new WebSocket('ws://localhost:8081/');
+  var socket = new WebSocket('ws://proyectillos.com:8081/');
   var players = [];
 
   socket.onopen = function () {
     socket.send(JSON.stringify({action: 'create-game'}));
   };
 
+  var shaking = false;
   socket.onmessage = function (message) {
 
     console.log(message.data);
@@ -30,6 +31,30 @@ var WsConnector = new function () {
         direction: message.speed > 0 ? -1 : 1,
         force: message.speed
       });
+    }
+
+    if (message.action === 'shake') {
+      if (shaking) {
+        return;
+      }
+      shaking = true;
+      setTimeout(function () {
+        shaking = false;
+      }, 1000)
+
+      the_game.events.push({
+        type: 'ball',
+        force: 10,
+        angle: {
+          x: Math.random()*2 - 1,
+          y: Math.random()*2 - 1
+        }
+      });
+
+      //$('canvas').css({ "position": "relative" });
+      for (var x = 1; x <= 3; x++) {
+        $('canvas').animate({ left: -25 }, 10).animate({ left: 0 }, 50).animate({ left: 25 }, 10).animate({ left: 0 }, 50);
+      }
     }
   };
 };
